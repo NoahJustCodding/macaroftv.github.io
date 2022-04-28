9d00e737f2f468310ba624f7c2393897b8666473
MacarofTv <40423590+MacarofTv@users.noreply.github.com>
2022-04-28 22:28:18 +0200

    Updates files via VSCode

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -10,3 +10,12 @@ jQuery(function(){
         });
     });
 });
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
