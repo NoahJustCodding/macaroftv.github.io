005bfd03bdbc4f18e07fdec45268551ee6979360
MacarofTv <40423590+MacarofTv@users.noreply.github.com>
2022-04-28 22:26:20 +0200

    Updates files via VSCode

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -1,7 +1,7 @@
 jQuery(function(){
     $(function () {
         $(window).scroll(function () {
-            if ($(this).scrollTop() > 200 ) { 
+            if ($(this).scrollTop() > 150 ) { 
                 $('#scrollUp').css('right','10px');
             } else { 
                 $('#scrollUp').removeAttr( 'style' );
